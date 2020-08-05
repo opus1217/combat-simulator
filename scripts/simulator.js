73d96ffb3a8f40309776e2b1aba15d369a44e2c4
opus1217 <jdpugh2@gmail.com>
2020-08-04 20:58:17 -0700

    0.0.8a 2020-08-04 Fixed to use client-side Simulate Combat setting (so no race problem) and correct argument calling.
    lang\en.json
    - Add CS5e.Simulate = "Simulate"
    
    scripts\simulator.js
    - Make number of simulations a slider
    - Change Simulate to a client setting
    - simplify calling of openForm to parallel group initiative
    
    templates\combat-tracker-add.html
    - Attempt to provide an extra button for Simulate (not used)

--- scripts/simulator.js
+++ scripts/simulator.js
@@ -1,4 +1,6 @@
 const MODULE_NAME = "combat-simulator"
+//Global variable for combatTrackerSimulate
+
 class CombatSimulator {
   static init() {
     game.settings.register(MODULE_NAME, "combatSimulatorVersion", {
@@ -12,10 +14,10 @@ class CombatSimulator {
     game.settings.register(MODULE_NAME, "combatTrackerSimulate", {
       name: game.i18n.localize("CS5e.SETTING.Simulate.Name"),
       hint: game.i18n.localize("CS5e.SETTING.Simulate.Hint"),
-      scope: "world",
+      scope: "client",
       config: true,
       default: false,
-      onChange: value => {replaceBeginCombat(game.combat)},
+      // onChange: value => {replaceBeginCombat(game.combat)},
       type: Boolean
     });
     game.settings.register(MODULE_NAME, "numberOfSimulations", {
@@ -24,7 +26,12 @@ class CombatSimulator {
       scope: "world",
       config: true,
       default: 100,
-      type: Number
+      type: Number,
+      range: {             // If range is specified, the resulting setting will be a range slider
+        min: 1,
+        max: 1000,
+        step: 100
+      }
     });
     game.settings.register(MODULE_NAME, "showCombatDetail", {
       name: game.i18n.localize("CS5e.SETTING.ShowCombatDetail.Name"),
@@ -65,26 +72,20 @@ class CombatSimulator {
 
   }
 
-  static openForm() {
-      if (this.form === undefined) {
-          this.form = new CombatSimulatorApplication();
-      }
-      this.form.setActiveCombat(game.combat)
-      this.form.render(true);
+  static simulateCombat() {
+    let shouldSimulateCombat = game.settings.get("combat-simulator","combatTrackerSimulate");
+
+    openForm();
   }
+
 }
 
-// Check the simulation settings every time; if not set then do the normal Begin Combat
-function simulateCombat() {
-  let shouldSimulateCombat = game.settings.get("combat-simulator","combatTrackerSimulate");
-
-  if (shouldSimulateCombat) {
-    CombatSimulator.openForm();
-  } else {
-    // do the normal combat
-    combat.originalStartCombat
-  }
-
+async function openForm() {
+    if (this.CSAForm === undefined) {
+        this.CSAForm = new CombatSimulatorApplication();
+    }
+    this.CSAForm.setActiveCombat(game.combat)
+    this.CSAForm.render(true);
 }
 
 /* Substitute for the Begin Combat button if user is GM
@@ -95,17 +96,27 @@ function simulateCombat() {
 */
 function replaceBeginCombat(combat, html) {
     if (!combat || !game.user.isGM) return;
-
-    //Try replacing/adding to the Begin Combat call in the Combat tracker
-    if (html) {
-           html.find("a[title='Begin Combat']").attr("title","Simulate Combat").text("Simulate Combat");
+    let shouldSimulateCombat = game.settings.get("combat-simulator","combatTrackerSimulate");
+    let beginCombatButtonTitle = game.i18n.localize("COMBAT.Begin");
+    let simulateCombatButtonTitle = game.i18n.localize("CS5e.Simulate");
+    //Replace the button and also the call in the Combat tracker
+    if (shouldSimulateCombat) {
+      if (!combat.originalStartCombat) {
+        combat.originalStartCombat =  combat.rollNPC;  //combat.startCombat;
+      }
+      combat.rollNPC = openForm.bind(combat);
+      //combat.startCombat = openForm.bind(combat);
+      if (html) {
+        html.find("a[title='" + beginCombatButtonTitle + "']").attr("title",simulateCombatButtonTitle).text(simulateCombatButtonTitle);
+      }
+    } else {
+      if (combat.originalStartCombat) {
+        combat.startCombat = combat.originalStartCombat;
+      }
+      if (html) {
+        html.find("a[title='" + simulateCombatButtonTitle + "']").attr("title",beginCombatButtonTitle).text(beginCombatButtonTitle);
+      }
     }
-
-    if (!combat.originalStartCombat) {
-      combat.originalStartCombat = combat.startCombat;
-    }
-    combat.startCombat = simulateCombat;
-
 }
 
 Hooks.on("init", CombatSimulator.init);
@@ -115,7 +126,29 @@ Hooks.on('setup', CombatSimulator.setup);
  * Override the startCombat method ("Begin Combat") from combat tracker.
  * TODO: Would like to override the button title or add another button but don't know how to do that
  */
-Hooks.on('renderCombatTracker', replaceBeginCombat);
+Hooks.on('renderCombatTracker', ({combat}, html) => {
+  if (!combat || !game.user.isGM) return;
+  let shouldSimulateCombat = game.settings.get("combat-simulator","combatTrackerSimulate");
+  let beginCombatButtonTitle = game.i18n.localize("COMBAT.Begin");
+  let simulateCombatButtonTitle = game.i18n.localize("CS5e.Simulate");
+  //Replace the button and also the call in the Combat tracker
+  if (shouldSimulateCombat) {
+    if (!combat.originalStartCombat) {
+      combat.originalStartCombat =  combat.startCombat;
+    }
+    combat.startCombat = openForm.bind(combat);
+    if (html) {
+      html.find("a[title='" + beginCombatButtonTitle + "']").attr("title",simulateCombatButtonTitle).text(simulateCombatButtonTitle);
+    }
+  } else {
+    if (combat.originalStartCombat) {
+      combat.startCombat = combat.originalStartCombat;
+    }
+    if (html) {
+      html.find("a[title='" + simulateCombatButtonTitle + "']").attr("title",beginCombatButtonTitle).text(beginCombatButtonTitle);
+    }
+  }
+});
 
 /**
  * Add the "Simulate Combat" as an option in the Combat Tracker settings
